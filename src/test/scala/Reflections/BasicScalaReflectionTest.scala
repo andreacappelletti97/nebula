491e928e358d028c02718f646150e0405904f234
andreacappelletti97 <andreacappelletti97@gmail.com>
2021-12-23 01:30:20 +0100

    [UPDATE]: actor system

---
+++ src/test/scala/Reflections/BasicScalaReflectionTest.scala
@@ -0,0 +1,21 @@
+package Reflections
+
+import Reflections.BasicScalaReflection.config
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class BasicScalaReflectionTest extends AnyFlatSpec with Matchers {
+  behavior of "configuration parameters module"
+
+  /*
+  it should "obtain the utilization ratio" in {
+    config.getDouble("simulation1.utilizationRatio") shouldBe 0.3E0
+  }
+
+  it should "obtain the MIPS capacity" in {
+    config.getLong("simulation1.dcNumber") shouldBe 2
+  }
+
+   */
+}
+
