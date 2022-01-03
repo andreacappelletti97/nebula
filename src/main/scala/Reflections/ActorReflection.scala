7ebff9b90c99a87101375a4d557bba34606ed853
andreacappelletti97 <andreacappelletti97@gmail.com>
2022-01-03 10:26:26 +0100

    [ADD]: First actor compilation unit

--- src/main/scala/Reflections/ActorReflection.scala
+++ src/main/scala/Reflections/ActorReflection.scala
@@ -1,8 +1,14 @@
 package Reflections
 
+import akka.actor.{Actor, ActorSystem, Props}
+
 import scala.reflect.runtime.currentMirror
 import scala.reflect.runtime.universe._
 import scala.tools.reflect.ToolBox
+
+abstract class CompiledActor(myName: String) extends Actor{
+  def receive: Receive
+}
 
 object ActorReflection extends App {
   val toolbox = currentMirror.mkToolBox()
@@ -10,16 +16,28 @@ object ActorReflection extends App {
   // Class instance
   val actorCode = q"""
   import akka.actor._
+  object HelloActor {
+   def props(name : String) = Props(new HelloActor(name))
+  }
 class HelloActor(myName: String) extends Actor {
   def receive = {
     case "hello" => println("hello from %s".format(myName))
     case _       => println("'huh?', said %s".format(myName))
   }
 }
-  val system = ActorSystem("HelloSystem")
-  val helloActor = system.actorOf(Props(new HelloActor("Fred")), name = "helloactor")
-  helloActor ! "hey"
+  //val system = ActorSystem("HelloSystem")
+  //val helloActor = system.actorOf(Props(new HelloActor("Fred")), name = "helloactor")
+  return HelloActor.props("Bobo")
 """
   val compiledCode = toolbox.compile(actorCode)()
+  println(compiledCode)
+  val actorSystem = ActorSystem("firstActorSystem")
+  val myProps = compiledCode.asInstanceOf[Props]
+  val helloActor = actorSystem.actorOf(myProps)
+  helloActor ! "hello"
+
+ // val myActor = compiledCode.asInstanceOf[CompiledActor]
+  //println(myActor)
+
 
 }
