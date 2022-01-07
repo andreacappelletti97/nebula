70a577acb04ab407e3195f529c71684782d3c859
andreacappelletti97 <andreacappelletti97@gmail.com>
2022-01-07 19:05:12 +0100

    [ADD]: Recursive population of ActorList

--- src/main/scala/Week1/HelloActorCompiler.scala
+++ src/main/scala/Week1/HelloActorCompiler.scala
@@ -5,7 +5,9 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.google.gson.Gson
+
 import java.util
+import scala.collection.mutable
 import scala.reflect.runtime.currentMirror
 import scala.tools.reflect.ToolBox
 
@@ -22,22 +24,32 @@ object HelloActorCompiler {
   //Create toolbox
   val toolbox = currentMirror.mkToolBox()
 
+  val gson = new Gson
+
+  val mapper = new ObjectMapper()
+  mapper.registerModule(DefaultScalaModule)
+
   case class MyActor(@JsonProperty("Name") myName: String, @JsonProperty("Case") myCases : Seq[String], @JsonProperty("Code") myCode : Seq[String])
 
+  /* This recursive function dynamically creates the PEs based on the application config */
+  def populateActors(actorsList : Seq[MyActor],n : Integer, myArray : util.ArrayList[Object], x: Integer) : Seq[MyActor] = {
+    if(n==0) actorsList
+    else {
+      val myjson = gson.toJson(myArray.get(x), classOf[util.LinkedHashMap[Any, Any]])
+      val data = mapper.readValue(myjson.stripMargin, classOf[MyActor])
+      populateActors(actorsList :+ data, n - 1, myArray, x + 1)
+    }
+    }
+
+
   def composeActors(configuration : Any) : Seq[MyActor] = {
-    val mySeq : Seq[MyActor] = Seq.empty[MyActor]
+    var mySeq: Seq[MyActor] = Seq.empty[MyActor]
     val myArray = configuration.asInstanceOf[util.ArrayList[Object]]
     //Create Gson
-    val gson = new Gson
-    val mapper = new ObjectMapper()
-    mapper.registerModule(DefaultScalaModule)
-    myArray.forEach(c => {
-      val myjson = gson.toJson(c, classOf[util.LinkedHashMap[Any, Any]])
-      println(myjson)
-      val data = mapper.readValue(myjson.stripMargin, classOf[MyActor])
-      println(data)
-    })
-    mySeq
+    val newActorsList: Seq[MyActor] = Seq.empty[MyActor]
+    val actorList : Seq[MyActor] = populateActors(newActorsList, 2, myArray, 0)
+    println(actorList)
+    actorList
   }
 }
 
