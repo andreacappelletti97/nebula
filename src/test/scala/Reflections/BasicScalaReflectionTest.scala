2fa73333dbed728a432131fb9184a8d5552d08bf
andreacappelletti97 <andreacappelletti97@gmail.com>
2022-01-07 11:35:09 +0100

    [ADD]: helloActor System init

--- src/test/scala/Reflections/BasicScalaReflectionTest.scala
+++ src/test/scala/Reflections/BasicScalaReflectionTest.scala
@@ -1,5 +1,6 @@
 package Reflections
 
+import Week1.HelloActor.config
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -7,14 +8,14 @@ class BasicScalaReflectionTest extends AnyFlatSpec with Matchers {
   behavior of "configuration parameters module"
 
   /*
-  it should "obtain the utilization ratio" in {
-    config.getDouble("simulation1.utilizationRatio") shouldBe 0.3E0
-  }
+    it should "obtain the utilization ratio" in {
+      config.getString("helloActor.yamlFile") shouldBe "HelloActor.yaml"
+    }
 
-  it should "obtain the MIPS capacity" in {
-    config.getLong("simulation1.dcNumber") shouldBe 2
-  }
+    it should "obtain the MIPS capacity" in {
+      config.getLong("simulation1.dcNumber") shouldBe 2
+    }
 
-   */
+     */
 }
 
