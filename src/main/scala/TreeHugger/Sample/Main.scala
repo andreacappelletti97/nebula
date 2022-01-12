5d8d5699e7d335b82ffc473d8c955a5e0064753c
andreacappelletti97 <andreacappelletti97@gmail.com>
2022-01-11 22:09:59 -0600

    [ADD]: Case Class Json reflection

--- src/main/scala/TreeHugger/Sample/Main.scala
+++ src/main/scala/TreeHugger/Sample/Main.scala
@@ -1,9 +1,10 @@
 package TreeHugger.Sample
 
-import HelperUtils.ObtainConfigReference
+import HelperUtils.{CreateLogger, ObtainConfigReference}
 import TreeHugger.Generator.TreehuggerGenerator
 import TreeHugger.Schema.TypeSchema
 
+class Main
 object Main extends App{
   //Init the config file to get static params
   val config = ObtainConfigReference("treeHugger") match {
@@ -11,9 +12,11 @@ object Main extends App{
     case None => throw new RuntimeException("Cannot obtain a reference to the config data.")
   }
 
+  //Init the logger
+  val logger = CreateLogger(classOf[Main])
+  logger.info("Init the JSON schema...")
   val schema = TypeSchema.fromJson(config.getString("treeHugger.jsonFile"))
+  logger.info("Parsing the JSON schema via TreeHugger...")
   println((new TreehuggerGenerator).generate(schema))
-
-
-
+  logger.info("Parsing completed")
 }
