3669e02a460c2b03f5e6d0b6d0f1279b594a7e92
andreacappelletti97 <andreacappelletti97@gmail.com>
2022-01-17 16:26:05 -0600

    [ADD]: Json modular architecture, Actors, ActorSystem and CaseClasses

--- src/main/scala/Nebula/Schema/CaseClassSchema.scala
+++ src/main/scala/Nebula/Schema/CaseClassSchema.scala
@@ -1,8 +1,31 @@
 package Nebula.Schema
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+
+import java.io.FileInputStream
+
 /***
-A case object is defined by a name without any argument
-A case class is defined by a name and some arguments
-Arguments are defined as @ArgumentSchema, that contains argName and argType
+ * A case object is defined by a name without any argument
+ * A case class is defined by a name and some arguments
+ * Arguments are defined as @ArgumentSchema, that contains argName and argType
  ***/
 case class CaseClassSchema(caseClassName: String, caseClassArgs: Option[Seq[ArgumentSchema]])
+
+object CaseClassSchema {
+  private val mapper = new ObjectMapper().registerModule(DefaultScalaModule)
+
+  /**
+   * Loads a schema from a JSON file.
+   */
+  def fromJson(fileName: String): Array[CaseClassSchema] = {
+    val inputStream = new FileInputStream(fileName)
+    try {
+      mapper.readValue(inputStream, classOf[Array[CaseClassSchema]])
+    } finally {
+      inputStream.close()
+    }
+  }
+
+
+}
