7d8bed84acc34ea8d7d2c01f427215fcdf6be34a
andreacappelletti97 <andreacappelletti97@gmail.com>
2022-02-03 09:47:01 -0600

    [ADD]: Schemas refactoring

--- src/main/scala/Nebula/Schema/CaseClassSchema.scala
+++ src/main/scala/Nebula/Schema/CaseClassSchema.scala
@@ -1,9 +1,5 @@
 package Nebula.Schema
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.scala.DefaultScalaModule
-
-import java.io.FileInputStream
 
 /***
  * A case object is defined by a name without any argument
@@ -11,21 +7,3 @@ import java.io.FileInputStream
  * Arguments are defined as @ArgumentSchema, that contains argName and argType
  ***/
 case class CaseClassSchema(caseClassName: String, caseClassArgs: Seq[ArgumentSchema])
-
-object CaseClassSchema {
-  private val mapper = new ObjectMapper().registerModule(DefaultScalaModule)
-
-  /**
-   * Loads a schema from a JSON file.
-   */
-  def fromJson(fileName: String): Array[CaseClassSchema] = {
-    val inputStream = new FileInputStream(fileName)
-    try {
-      mapper.readValue(inputStream, classOf[Array[CaseClassSchema]])
-    } finally {
-      inputStream.close()
-    }
-  }
-
-
-}
