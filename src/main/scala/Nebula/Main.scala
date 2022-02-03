44bb375f28564864ee04fe4a25775c815cd29e06
andreacappelletti97 <andreacappelletti97@gmail.com>
2022-02-03 15:32:22 -0600

    [ADD]: Case classes and case objects generator without quasiquotes

--- src/main/scala/Nebula/Main.scala
+++ src/main/scala/Nebula/Main.scala
@@ -2,7 +2,7 @@ package Nebula
 
 import HelperUtils.{CreateLogger, ObtainConfigReference}
 import Nebula.Compiler.{ActorCompiler, CaseClassCompiler}
-import Nebula.Generator.{ActorCodeGenerator, ActorGenerator, ActorSystemGenerator, CaseClassGenerator}
+import Nebula.Generator.{ActorCodeGenerator, ActorGenerator, ActorSystemGenerator, CaseClassCodeGenerator, CaseClassGenerator}
 import Nebula.Parser.JSONParser
 import akka.actor.{ActorSystem, Props}
 
@@ -41,7 +41,8 @@ object Main extends  App{
   //Generate Actors from the JSON schema
   val actorsJson = JSONParser.getActorSchemaFromJson(config.getString("nebula.actorsJsonFile"))
 
-  generateActors()
+
+  //generateActors()
 
   def generateActorSystem(): Unit = {
     //Get all the Actor Props compilation units
