a9944d9c4a5d9760c5fe3ebf92ef5d375a3cfa60
andreacappelletti97 <andreacappelletti97@gmail.com>
2022-02-12 20:13:32 -0600

    [ADD]: Logger, Yaml support and remote config

--- src/main/scala/Nebula/Main.scala
+++ src/main/scala/Nebula/Main.scala
@@ -4,10 +4,9 @@ import HelperUtils.ObtainConfigReference
 import NebulaScala2.Compiler.{ActorCodeCompiler, MessageCodeCompiler, ToolboxGenerator}
 import NebulaScala2.{Compiler, Scala2Main}
 import NebulaScala3.Generator.{ActorCodeGenerator, MessageCodeGenerator}
-import NebulaScala3.Parser.JSONParser
+import NebulaScala3.Parser.{JSONParser, YAMLParser}
 import NebulaScala3.Scala3Main
-import org.slf4j.LoggerFactory
-
+import com.typesafe.scalalogging.Logger
 class Main
 
 object Main:
@@ -16,14 +15,18 @@ object Main:
     case Some(value) => value
     case None => throw new RuntimeException("Cannot obtain a reference to the config data.")
   }
+  //Logger init
+  val logger = Logger("Main")
+  //Main method of the framework
   @main def nebulaMain: Unit =
-    println(Scala2Main.scala2Message)
-    println(Scala3Main.scala3Message)
+    logger.info(Scala2Main.scala2Message)
+    logger.info(Scala3Main.scala3Message)
     //Get the current Toolbox from the Scala2 APIs
     val toolbox = ToolboxGenerator.generateToolbox()
     //Generate Actors from the JSON schema
     val actorsJson = JSONParser.getActorSchemaFromJson(config.getString("nebula.actorsJsonFile"))
-    val messagesJson = JSONParser.getCaseClassSchemaFromJson(config.getString("nebula.messagesJsonFile"))
+    val messagesJson = JSONParser.getMessagesSchemaFromJson(config.getString("nebula.messagesJsonFile"))
+
 
     toolbox.eval(
       toolbox.parse(
