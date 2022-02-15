b3077b62cead9b20d622ec9e551ba7e2fc988d33
andreacappelletti97 <andreacappelletti97@gmail.com>
2022-02-14 19:05:25 -0600

    [ADD]: Actor ping pong example

--- nebula_scala2/src/main/scala/NebulaScala2/Compiler/ActorCodeCompiler.scala
+++ nebula_scala2/src/main/scala/NebulaScala2/Compiler/ActorCodeCompiler.scala
@@ -1,18 +1,26 @@
 package NebulaScala2.Compiler
 
-import akka.actor.{ActorSystem, Props}
+import NebulaScala2.SimpleActor
 import scala.tools.reflect.ToolBox
+import akka.actor.{Props, ActorRef, ActorSystem}
 
 class ActorCodeCompiler
 
 object ActorCodeCompiler {
+  var reference : ActorRef = _
 
   def runCode(codeToRun : String, toolbox:  ToolBox[scala.reflect.runtime.universe.type]): Unit ={
+    //Compile Actor code and get the Props object
     val tree = toolbox.parse(codeToRun)
     val actorProps =  toolbox.compile(tree)().asInstanceOf[Props]
+    //Define ActorSystem
     val actorSystem = ActorSystem("system")
-    val helloActor = actorSystem.actorOf(actorProps)
-    helloActor ! "changeX"
+
+    val myActor = actorSystem.actorOf(Props[SimpleActor], "SimpleActor")
+    reference = myActor
+    val helloActor = actorSystem.actorOf(actorProps, "HelloActor")
+    helloActor ! "getReference"
+    //helloActor ! "sendMessage"
     //actorSystem.terminate()
   }
 
