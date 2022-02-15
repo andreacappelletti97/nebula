8bac941cf76ebbc87b0559cee8405a6a24b64db9
andreacappelletti97 <andreacappelletti97@gmail.com>
2022-02-14 19:09:07 -0600

    [ADD]: Actor ping pong example

--- nebula_scala2/src/main/scala/NebulaScala2/SimpleActor.scala
+++ nebula_scala2/src/main/scala/NebulaScala2/SimpleActor.scala
@@ -4,10 +4,10 @@ import akka.actor.Actor
 
 class SimpleActor extends Actor {
   override def receive: Receive = {
-    case _ => println("I have received something!")
-    case "hello" =>
-      Thread.sleep(2000) 
-      sender() ! "getReference" 
+    case _ =>  
+      println("I have received something!")
+      Thread.sleep(2000)
+      sender() ! "getReference"
   }
 }
 
