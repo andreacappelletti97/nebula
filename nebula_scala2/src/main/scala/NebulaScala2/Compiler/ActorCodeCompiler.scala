14864f9a0a9b1bb5eec5b95bd8af687f782038b6
andreacappelletti97 <andreacappelletti97@gmail.com>
2022-02-17 00:16:02 -0600

    [ADD]: Cinnamon custom metrics API

--- nebula_scala2/src/main/scala/NebulaScala2/Compiler/ActorCodeCompiler.scala
+++ nebula_scala2/src/main/scala/NebulaScala2/Compiler/ActorCodeCompiler.scala
@@ -1,8 +1,15 @@
 package NebulaScala2.Compiler
 
 import NebulaScala2.SimpleActor
+
 import scala.tools.reflect.ToolBox
-import akka.actor.{Props, ActorRef, ActorSystem}
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import com.lightbend.cinnamon.akka.CinnamonMetrics
+import com.lightbend.cinnamon.metric.Counter
+import com.lightbend.cinnamon.metric.Recorder
+import com.lightbend.cinnamon.metric.GaugeDouble
+import com.lightbend.cinnamon.metric.GaugeLong
+import com.lightbend.cinnamon.metric.Rate
 
 class ActorCodeCompiler
 
@@ -15,14 +22,47 @@ object ActorCodeCompiler {
     val actorProps =  toolbox.compile(tree)().asInstanceOf[Props]
     //Define ActorSystem
     val actorSystem = ActorSystem("system")
+    createActorSystemMetrics(actorSystem)
 
-    val myActor = actorSystem.actorOf(Props[SimpleActor], "SimpleActor")
-    reference = myActor
+    val simpleActor = actorSystem.actorOf(Props[SimpleActor], "SimpleActor")
+    reference = simpleActor
     val helloActor = actorSystem.actorOf(actorProps, "HelloActor")
     helloActor ! "getReference"
     //helloActor ! "sendMessage"
     //actorSystem.terminate()
   }
+
+  def createActorSystemMetrics(actorSystem: ActorSystem) : Unit = {
+    val sysCounter: Counter = CinnamonMetrics(actorSystem).createCounter("sysCounter")
+    val sysGaugeDouble: GaugeDouble = CinnamonMetrics(actorSystem).createGaugeDouble("sysGaugeDouble")
+    val sysGaugeLong: GaugeLong = CinnamonMetrics(actorSystem).createGaugeLong("sysGaugeLong")
+    //val sysProvidingGaugeDouble: ProvidingGaugeDouble = CinnamonMetrics(actorSystem).createProvidingGaugeDouble("sysProvidingGaugeDouble", doubleValueProvider)
+    //val sysProvidingGaugeLong: ProvidingGaugeLong = CinnamonMetrics(actorSystem).createProvidingGaugeLong("sysProvidingGaugeLong", longValueProvider)
+    val sysRate: Rate = CinnamonMetrics(actorSystem).createRate("sysRate")
+    val sysRecorder: Recorder = CinnamonMetrics(actorSystem).createRecorder("sysRecorder")
+  }
+
+  def createActorMetrics(actor : Actor) : Unit = {
+    val counter: Counter = CinnamonMetrics(actor.context).createCounter("counter")
+    val gaugeDouble: GaugeDouble = CinnamonMetrics(actor.context).createGaugeDouble("gaugeDouble")
+    val gaugeLong: GaugeLong = CinnamonMetrics(actor.context).createGaugeLong("gaugeLong")
+    //val providingGaugeDouble: ProvidingGaugeDouble = CinnamonMetrics(actor.context).createProvidingGaugeDouble("providingGaugeDouble", doubleValueProvider)
+    //val providingGaugeLong: ProvidingGaugeLong = CinnamonMetrics(actor.context).createProvidingGaugeLong("providingGaugeLong", longValueProvider)
+    val rate: Rate = CinnamonMetrics(actor.context).createRate("rate")
+    val recorder: Recorder = CinnamonMetrics(actor.context).createRecorder("recorder")
+  }
+
+/*
+  val doubleValueProvider: DoubleValueProvider = new DoubleValueProvider {
+    val cnt: AtomicReference[Double] = new AtomicReference(0.0)
+    override def currentValue(): Double = cnt.get()
+  }
+
+  val longValueProvider: LongValueProvider = new LongValueProvider {
+    val cnt: AtomicLong = new AtomicLong(0)
+    override def currentValue(): Long = cnt.getAndIncrement()
+  }
+*/
 
 }
 
