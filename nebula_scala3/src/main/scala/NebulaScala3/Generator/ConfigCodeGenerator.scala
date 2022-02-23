0feaff4318eec0244b019ebe9c7dd616d2583a50
andreacappelletti97 <andreacappelletti97@gmail.com>
2022-02-23 11:01:41 -0600

    [ADD]: Config injection at run-time schema and generator

--- nebula_scala3/src/main/scala/NebulaScala3/Generator/ConfigCodeGenerator.scala
+++ nebula_scala3/src/main/scala/NebulaScala3/Generator/ConfigCodeGenerator.scala
@@ -7,5 +7,29 @@ class ConfigCodeGenerator
 object ConfigCodeGenerator:
   //This function generates the config settings for the ActorSystems
   def generateConfigCode(configSchemaList : Seq[CinnamonMonitoringSchema]) : Seq[String] = ???
+  
+  private def generateConfig(configSchema : CinnamonMonitoringSchema) : String =
+    s"""cinnamon {
+      |${consoleReporter(configSchema.consoleReporter)}
+      |}
+      |${prometheusHttp(configSchema.prometheusHttpServer)}
+      |""".stripMargin
+  
+  //Enable in the entire JVM
+  private def prometheusHttp(enabled : Boolean) =
+    if(enabled) """cinnamon.prometheus {
+                  |  exporters += http-server
+                  |}""".stripMargin
+      
+  //Enable in the entire JVM
+  private def consoleReporter(enabled : Boolean) =
+    if(enabled) """chmetrics = {
+                  |    reporters += "console-reporter"
+                  |  }""".stripMargin
+
+
+    
+
+      
     
   
