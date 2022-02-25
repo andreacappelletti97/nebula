ddc18ade25af26c57c51b6ffbd63ae190cd6052e
andreacappelletti97 <andreacappelletti97@gmail.com>
2022-02-24 18:55:00 -0600

    [ADD]: Dynamic Message wrapper

--- nebula_scala3/src/main/scala/NebulaScala3/Scala3Main.scala
+++ nebula_scala3/src/main/scala/NebulaScala3/Scala3Main.scala
@@ -1,5 +1,7 @@
 package NebulaScala3
 
+import com.github.os72.protobuf.dynamic.DynamicSchema
+
 //In this object we declare all the global mutable variables
 object Scala3Main:
   val scala3Message = "Hello from Nebula Scala 3! :)"
@@ -7,3 +9,6 @@ object Scala3Main:
   //Message definition
   trait SomeTrait
   var theKeeper: SomeTrait = _
+
+  //This wrapper contains the dynamic protobuf builders to instantiate at run-time
+  var dynamicMessagesBuilders : Seq[DynamicSchema] = _
