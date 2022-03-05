fdc5742c9cfc1b31b9ac8b569e1c18ba3d5fb43d
andreacappelletti1997 <andrea@aampe.com>
2022-03-04 20:28:58 -0600

    [ADD]: Protobuf generator

--- nebula_scala3/src/main/scala/NebulaScala3/Generator/ProtoMessageGenerator.scala
+++ nebula_scala3/src/main/scala/NebulaScala3/Generator/ProtoMessageGenerator.scala
@@ -1,28 +1,38 @@
 package NebulaScala3.Generator
 
-import NebulaScala3.Schema.MessageSchema
+import NebulaScala3.Schema.{ArgumentSchema, MessageSchema}
+import NebulaScala3.message.ProtoMessage
 
 
 class ProtoMessageGenerator
 
-object ProtoMessageGenerator
-  /*
+object ProtoMessageGenerator {
+
+    def runIt : Unit = {
+      println(generateProtoMessages(
+        Array(MessageSchema("Auth", Seq(ArgumentSchema("email", "String", "")))),
+        0,
+        Seq.empty
+      ))
+    }
+
   def generateProtoMessages(messageList: Array[MessageSchema],
-                            iterator : Int,
-                            protoMessages : Seq[String]
-                             ) : Seq[String] =
-    if(iterator >= messageList.length) protoMessages
+                            iterator: Int,
+                            protoMessages: Seq[ProtoMessage]
+                           ): Seq[ProtoMessage] = {
+    if (iterator >= messageList.length) protoMessages
     else generateProtoMessages(
       messageList, iterator + 1, protoMessages :+
         generateMessage(messageList(iterator))
     )
+  }
 
-
-    def generateMessage(message : MessageSchema): String = {
+    def generateMessage(message: MessageSchema): ProtoMessage = {
+      var argMap : Map[String, String] = Map()
       message.messageArgs.foreach(arg
-      => println(arg)
+      => argMap += (arg.argName -> arg.argType)
       )
-      "ciao"
+      return ProtoMessage(message.messageName,argMap)
     }
-  */
+}
 
