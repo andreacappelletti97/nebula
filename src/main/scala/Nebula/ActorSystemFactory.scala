0ac0bae7a6257215a977e98a1dbf79cfa1f082d8
andreacappelletti <andrea@aampe.com>
2022-03-11 17:25:16 -0600

    [ADD]: Akka Clustering Sharding factory

--- src/main/scala/Nebula/ActorSystemFactory.scala
+++ src/main/scala/Nebula/ActorSystemFactory.scala
@@ -1,7 +1,8 @@
 package Nebula
 
-import akka.actor.ActorSystem
+import akka.actor.{ActorSystem, Props}
 import NebulaScala2.Scala2Main.generatedActorSystems
+import com.typesafe.config.ConfigFactory
 
 class ActorSystemFactory
 
@@ -10,5 +11,26 @@ object ActorSystemFactory {
   def initActorSystem(name : String): Unit = {
     val actorSystem = ActorSystem(name)
     generatedActorSystems += name -> actorSystem
+  }
+
+  //This function start the Akka Cluster Sharding Nebula scenario
+  def startCluster(ports : List[Int]): Unit = {
+    def startCluster(ports : List[Int]): Unit = {
+      ports.foreach{port =>
+        val config = ConfigFactory.parseString(
+          s"""
+             |akka.remote.artery.canonical.port = $port
+             |""".stripMargin
+        ).withFallback(
+          ConfigFactory.load(
+            "clustering/cluster.conf"
+          )
+        )
+        //All the actorSystem names in a cluster should be the same
+        val system = ActorSystem("Nebula", config)
+      }
+    }
+    startCluster(List(2551, 2552, 0))
+    //0 -> system allocates the port for you
   }
 }
