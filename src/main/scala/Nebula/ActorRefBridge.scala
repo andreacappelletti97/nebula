4b1ff98350f4f09127cc7eb074acd3e639dee9f5
andreacappelletti <andrea@aampe.com>
2022-03-21 14:40:47 -0500

    [ADD]: ActorRef bridge

---
+++ src/main/scala/Nebula/ActorRefBridge.scala
@@ -0,0 +1,16 @@
+package Nebula
+
+import NebulaScala2.Scala2Main
+import NebulaScala2.Scala2Main.generatedActorsRef
+import akka.actor.ActorRef
+object ActorRefBridge {
+  def getActorRef(transitions : Seq[String], iterator: Int, actorRefList: Seq[ActorRef]) : Seq[ActorRef] = {
+    if (iterator >= transitions.size) actorRefList
+    else {
+      generatedActorsRef.get(transitions(iterator)) match {
+        case Some(actorRef) => getActorRef(transitions, iterator + 1, actorRefList :+ actorRef)
+        case None => getActorRef(transitions, iterator + 1, actorRefList)
+      }
+    }
+  }
+}
