cb98932df4edf3bb21c2fdc8fe316a05e0c4057f
andreacappelletti <andrea@aampe.com>
2022-03-22 16:38:53 -0500

    [ADD]: Comments and logging

--- src/main/scala/Nebula/Main.scala
+++ src/main/scala/Nebula/Main.scala
@@ -24,20 +24,21 @@ object Main:
   }
   //Logger init
   val logger = Logger("Main")
+
   //Main method of the framework
   @main def nebulaMain: Unit =
     logger.info(Scala2Main.scala2Message)
     logger.info(Scala3Main.scala3Message)
     //Init Kamon monitoring instrumentation
-    if(config.getBoolean("nebula.enableKamon")) Scala2Main.initKamon()
+    if (config.getBoolean("nebula.enableKamon")) Scala2Main.initKamon()
     //Get the current Toolbox from the Scala2 APIs
     val toolbox = ToolboxGenerator.generateToolbox()
 
     //TODO: add yaml configuration
     //Generate Actors from the JSON schema
-    val actorsJson = if(config.getBoolean("nebula.buildArtifact")) JSONParser.getActorSchemaFromJson(config.getString("nebula.actorsBuildJsonFile"))  else  JSONParser.getActorSchemaFromJson(config.getString("nebula.actorsJsonFile"))
-    val messagesJson = if(config.getBoolean("nebula.buildArtifact")) JSONParser.getMessagesSchemaFromJson(config.getString("nebula.messagesBuildJsonFile")) else JSONParser.getMessagesSchemaFromJson(config.getString("nebula.messagesJsonFile"))
-    val orchestratorJson = if(config.getBoolean("nebula.buildArtifact")) JSONParser.getOrchestratorFromJson(config.getString("nebula.orchestratorBuildJsonFile"))  else  JSONParser.getOrchestratorFromJson(config.getString("nebula.orchestratorJsonFile"))
+    val actorsJson = if (config.getBoolean("nebula.buildArtifact")) JSONParser.getActorSchemaFromJson(config.getString("nebula.actorsBuildJsonFile")) else JSONParser.getActorSchemaFromJson(config.getString("nebula.actorsJsonFile"))
+    val messagesJson = if (config.getBoolean("nebula.buildArtifact")) JSONParser.getMessagesSchemaFromJson(config.getString("nebula.messagesBuildJsonFile")) else JSONParser.getMessagesSchemaFromJson(config.getString("nebula.messagesJsonFile"))
+    val orchestratorJson = if (config.getBoolean("nebula.buildArtifact")) JSONParser.getOrchestratorFromJson(config.getString("nebula.orchestratorBuildJsonFile")) else JSONParser.getOrchestratorFromJson(config.getString("nebula.orchestratorJsonFile"))
     //Dynamic
     //val dynamicActorJson = if(config.getBoolean("nebula.buildArtifact")) JSONParser.getDynamicActorsFromJson(config.getString("nebula.actorsDynamicJsonFile")) else JSONParser.getDynamicActorsFromJson(config.getString("nebula.actorsDynamicJsonFile"))
     //val dynamicMessageJson = if(config.getBoolean("nebula.buildArtifact")) JSONParser.getDynamicMessagesFromJson(config.getString("nebula.messagesDynamicJsonFile")) else JSONParser.getDynamicMessagesFromJson(config.getString("nebula.messagesDynamicJsonFile"))
@@ -52,14 +53,17 @@ object Main:
     //val dynamicActor = DynamicActorGenerator.generateActorCode(dynamicActorJson, 0, Seq.empty)
     //Store the generated actor props into a global state for orchestration
 
+    //Generate ActorCode as String
     val actorCode = ActorCodeGeneratorOrchestration.generateActorCode(actorsJson, 0, Seq.empty)
     logger.info("Actors code have been generated...")
     println(actorCode)
 
+    //Generate ActorProps
     val generatedActors = ActorCodeCompiler.compileActors(actorCode, toolbox, 0, Seq.empty)
     logger.info("Actors code have been compiled into Props objects...")
 
-    generatedActors.zipWithIndex.foreach{
+    //Store ActorProps into the global state of Nebula
+    generatedActors.zipWithIndex.foreach {
       case (actor, index) =>
         generatedActorsProps += actorsJson(index).actorName.toLowerCase -> actor
     }
@@ -70,86 +74,89 @@ object Main:
     //Generate messages within the standard ProtoBuffer
     val protoMessages = ProtoMessageGenerator.generateProtoMessages(
       messagesJson,
-0,
+      0,
       Seq.empty
     )
-    protoMessages.foreach(message => protoBufferList +=  message.name.toLowerCase -> message)
+    //Store generated ProtoBuffer into the global state of Nebula
+    protoMessages.foreach(message => protoBufferList += message.name.toLowerCase -> message)
     logger.info("ProtoMessages have been generated...")
     println(protoBufferList)
 
-    val actorSystem : ActorSystem = ActorSystemFactory.initActorSystem("system")
+    val actorSystem: ActorSystem = ActorSystemFactory.initActorSystem("system")
 
-    orchestratorJson.foreach{actor =>
-    val actorProps = generatedActorsProps.getOrElse(actor.name.toLowerCase, return)
-    val actorRef: ActorRef = ActorFactory.initActor(actorSystem, actorProps, actor.name)
+    //Start Nebula orchestration --> init actors from actorProps stored
+    orchestratorJson.foreach { actor =>
+      val actorProps = generatedActorsProps.getOrElse(actor.name.toLowerCase, return)
+      val actorRef: ActorRef = ActorFactory.initActor(actorSystem, actorProps, actor.name)
       generatedActorsRef = generatedActorsRef += (actor.name.toLowerCase -> actorRef)
     }
     logger.info("End of ActorRef init ...")
     println(generatedActorsRef)
     logger.info("Sending init messages...")
 
+    //Send init messages --> send init messages to the Actor instantiated
     orchestratorJson.foreach { orchestration =>
-    val actor = generatedActorsRef.getOrElse(orchestration.name.toLowerCase, return)
-    orchestration.initMessages.foreach { message =>
-      val protoMessage = protoBufferList.getOrElse(message.toLowerCase, return)
-      actor ! protoMessage
+      val actor = generatedActorsRef.getOrElse(orchestration.name.toLowerCase, return)
+      orchestration.initMessages.foreach { message =>
+        val protoMessage = protoBufferList.getOrElse(message.toLowerCase, return)
+        actor ! protoMessage
+      }
     }
-    }
 
-    /*
-
-
-    val message = protoBufferList.getOrElse("authentication", return)
-    println(message)
-    val actorRef = generatedActorsRef.getOrElse("firstactor", return)
-    println(actorRef)
-    MessageSender.sendMessage(actorRef, message)
-
-    */
+/*
 
 
-    /*
-    val firstMessage = protoBufferList.get("init")
-    println("first message is: " + firstMessage)
-    println("second message is: " + protoBufferList.get("authentication"))
+val message = protoBufferList.getOrElse("authentication", return)
+println(message)
+val actorRef = generatedActorsRef.getOrElse("firstactor", return)
+println(actorRef)
+MessageSender.sendMessage(actorRef, message)
 
-    MessageSender.sendMessage(generatedActorsProps("actorName"), protoBufferList("authentication"))
-    //MessageSender.sendMessage(generatedActorsProps("actorName"), protoBufferList("init"))
+*/
 
-    Thread.sleep(3000)
-    println("X is now ... " + NebulaScala3.Scala3Main.xxx)
 
-    println("Orchestrator JSON is")
-    orchestratorJson.foreach(o => println(o))
-    */
+/*
+val firstMessage = protoBufferList.get("init")
+println("first message is: " + firstMessage)
+println("second message is: " + protoBufferList.get("authentication"))
 
-    /*
-    val proto = ProtoMessage("ciao", Map("AL" -> "Alabama", "AK" -> "Alaska"))
-    println(proto.content)
-    println(proto.args)*/
+MessageSender.sendMessage(generatedActorsProps("actorName"), protoBufferList("authentication"))
+//MessageSender.sendMessage(generatedActorsProps("actorName"), protoBufferList("init"))
 
-    /*
+Thread.sleep(3000)
+println("X is now ... " + NebulaScala3.Scala3Main.xxx)
 
-    toolbox.eval(
-      toolbox.parse(
-      """case class Film(title: String) extends NebulaScala3.Scala3Main.SomeTrait;
-          |NebulaScala3.Scala3Main.theKeeper = Film("Kingdom")""".stripMargin)
-    )
-    println(NebulaScala3.Scala3Main.theKeeper)
+println("Orchestrator JSON is")
+orchestratorJson.foreach(o => println(o))
+*/
 
-    val actorCode = ActorCodeGenerator.generateActorCode(actorsJson, 0, Seq.empty)
-    println(actorCode)
+/*
+val proto = ProtoMessage("ciao", Map("AL" -> "Alabama", "AK" -> "Alaska"))
+println(proto.content)
+println(proto.args)*/
 
-    val actorProps = ActorCodeCompiler.compileActors(actorCode, toolbox, 0, Seq.empty)
-    println(actorProps)
-    
-    ActorCodeCompiler.runExample(actorProps(0), toolbox)
-    */
+/*
 
-    //println(s"xxx = ${Scala3Main.xxx}")
-    //Run the code
-    //Thread.sleep(3000)
-    //println(s"xxx = ${Scala3Main.xxx}")
+toolbox.eval(
+  toolbox.parse(
+  """case class Film(title: String) extends NebulaScala3.Scala3Main.SomeTrait;
+      |NebulaScala3.Scala3Main.theKeeper = Film("Kingdom")""".stripMargin)
+)
+println(NebulaScala3.Scala3Main.theKeeper)
+
+val actorCode = ActorCodeGenerator.generateActorCode(actorsJson, 0, Seq.empty)
+println(actorCode)
+
+val actorProps = ActorCodeCompiler.compileActors(actorCode, toolbox, 0, Seq.empty)
+println(actorProps)
+
+ActorCodeCompiler.runExample(actorProps(0), toolbox)
+*/
+
+//println(s"xxx = ${Scala3Main.xxx}")
+//Run the code
+//Thread.sleep(3000)
+//println(s"xxx = ${Scala3Main.xxx}")
 
 
 
