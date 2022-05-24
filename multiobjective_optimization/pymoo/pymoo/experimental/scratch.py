bc95b74eb2b312822157514dedca330dd9d08435
andreacappelletti <andrea@aampe.com>
2022-05-24 10:46:07 +0200

    Add ML module

--- multiobjective_optimization/pymoo/pymoo/experimental/scratch.py
+++
@@ -1,5 +0,0 @@
-from pymoo.factory import MODAct
-from pymoo.util.plotting import plot
-
-problem = MODAct("cs3")
-plot(problem.pareto_front(), no_fill=True)
--- multiobjective_optimization/pymoo/pymoo/operators/mutation/nom.py
+++
@@ -1,7 +0,0 @@
-from pymoo.core.mutation import Mutation
-
-
-class NoMutation(Mutation):
-
-    def _do(self, problem, X, **kwargs):
-        return X
